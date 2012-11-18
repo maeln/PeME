1f7afa05ec1a53ec62060b172ac5eff629d054dc
maeln <contact@maeln.com>
2012-11-18 14:29:36 +0100

    Add support for UV texture and fix some mistakes.

--- PeME.cxx
+++ PeME.cxx
@@ -17,6 +17,10 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 // MA 02110-1301, USA.
 
+// TO DO:
+	// - Texture coordinate.
+	// - adapting the default name if more than 1 mesh had no name.
+	// - Add the support of compression via zlib.
 
 #include <iostream>
 #include <fstream>
@@ -34,7 +38,7 @@ void write_mesh(aiMesh* mesh, std::string filename)
 	unsigned int numVert = mesh->mNumVertices;
 	// unsigned int numNormals = mesh.mNormals.Length(); // mNormals is the size of mNumVertices, see the documentation.
 	aiString name = mesh->mName;
-	if(name.length == 0)
+	if(name.length == 0) // On évite que le nom du mesh soit vide et rendent un fichier invalide.
 	{
 		std::cerr << "Your mesh has no name and will be named 'mesh' by default." << std::endl;
 		name = "mesh";
@@ -43,25 +47,51 @@ void write_mesh(aiMesh* mesh, std::string filename)
 	aiVector3D* normals = mesh->mNormals;
 	aiVector3D* vertices = mesh->mVertices;
 	
-	std::ofstream file(filename.c_str(), std::ios::out | std::ios::ate);
+	std::ofstream file(filename.c_str(), std::ios::out | std::ios::ate); // On ajoute les arrays en fin de fichier, évite de supprimer tout le fichier si plusieurs mesh dans le même fichier.
 	if(file)
 	{
 		file << "// Vertices of the meshs" << std::endl;
-		file << "float " << name.C_Str() << "[] = {" << std::endl;
-		for(unsigned int n = 0; n <= numVert; n++)
+		file << "float " << name.C_Str() << "[] = {" << std::endl; // On créer un tableaux du type : float array[] = { x, y, z, ... };
+		for(unsigned int n = 0; n <= (numVert-1); n++)
 		{
 			file << "\t" << vertices[n].x << "," << vertices[n].y << "," << vertices[n].z << "," << std::endl;
 		}
 		file << "};" << std::endl;
 		
-		file << "// Normals of the meshs" << std::endl;
+		file << "// Normals of the meshs" << std::endl; // Pareille pour les normales.
 		file << "float " << name.C_Str() << "_normals" << "[] = {" << std::endl;
-		for(unsigned int n = 0; n <= numVert; n++)
+		for(unsigned int n = 0; n <= (numVert-1); n++)
 		{
 			file << "\t" << normals[n].x << "," << normals[n].y << "," << normals[n].z << "," << std::endl;
 		}
 		file << "};" << std::endl;
 		
+		if(mesh->HasTextureCoords(0)) // Si le fichier à des coordonnées pour les texture on les écrits aussi 
+		{
+			unsigned int* nbUV = mesh->mNumUVComponents;
+			aiVector3D** uvCoord = mesh->mTextureCoords;
+			for(unsigned int i = 0; i < 0x8; i++)
+			{
+				if(nbUV[i] != 0)
+				{
+					file << "// Textcoords for UV channel " << i << std::endl; // Pareille pour les normales.
+					file << "unsigned int " << name.C_Str() << "_textDimension" << i << "(" << nbUV[i] << ");" << std::endl;
+					file << "float " << name.C_Str() << "_texcords" << i << "[] = {" << std::endl;
+					for(unsigned int n = 0; n <= (numVert-1); n++)
+					{
+						file << "\t";
+						file << uvCoord[i][n].x << ",";
+						if(nbUV[0] >= 2)
+							file << uvCoord[i][n].y << ",";
+						if(nbUV[0] >= 3)
+							file << uvCoord[i][n].z << ",";
+						
+						file << std::endl;
+					}
+					file << "};" << std::endl;
+				}
+			}
+		}
 		file.close();
 	}
 	else
@@ -70,14 +100,18 @@ void write_mesh(aiMesh* mesh, std::string filename)
 
 int main(int argc, char **argv)
 {
+	// commande :
+	// PeME [-s] <fichier 3D> <fichier C de sortie>
+	
 	if(argc > 1)
 	{
 		Assimp::Importer importer;
 		const aiScene* scene;
-		bool third;
+		bool third; // On veut vérifier si le fichier de sortie est le 3eme argument ( c'est le cas si on utilise -s ).
 		
 		if(argv[1] == "-s")
 		{
+			// On lit le fichier, on décompose toute les faces en triangles, on joint les vertices identique ( normalement plus besoin d'index suite à ça ) et on génère les normales.
 			scene = importer.ReadFile(argv[2], aiProcess_CalcTangentSpace | aiProcess_Triangulate | aiProcess_JoinIdenticalVertices | aiProcess_GenSmoothNormals | aiProcess_SortByPType );
 			third = true;
 		}
@@ -92,13 +126,18 @@ int main(int argc, char **argv)
 		{
 			unsigned int numMeshes = scene->mNumMeshes;
 			aiMesh** dispMesh = scene->mMeshes;
-			for(unsigned int n = 0; n < numMeshes; n++)
+			for(unsigned int n = 0; n < numMeshes; n++) // Pour chaque mesh de la scène on va écrire un array de vertices et de normales.
 			{
 				if(third)
 					write_mesh(dispMesh[n], argv[3]);
 				else
 					write_mesh(dispMesh[n], argv[2]);
 			}
+		}
+		else
+		{
+			std::runtime_error("No meshes.");
+			return 1;
 		}
 	}
 	else
